ee784867dde32da11bff12ffd5f59856010c242b
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-05-17 09:54:06 +0200

    complete version 0.0.3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let alert = '<div class="alert alert-warning" role="alert">'+
   'Seems like if your device/browser do not support accelerometer. Try to change device/browser.'+
   '</div>';
 
-var activityTopic = ""; //pattern sensor/{client id}/position
+var activityTopic = ""; //pattern sensor/{client id}/activity
 var sensorPubTopic = ""; //pattern sensor/{client id}/accelerometer
 var clientUniqueId = "";
 
@@ -22,12 +22,12 @@ $(document).ready(async function() {
     
     try{ // Paho configuration
       
-      let location = {hostname:"192.168.1.47", port:"16396", awsClientID: "client"};
+      let location = {hostname:"192.168.1.63", port:"16396", awsClientID: "client"};
       client = new Paho.MQTT.Client(location.hostname, Number(location.port),"/wss", location.awsClientID);
       
       // Init parameters
       var clientUniqueId = await getUniqueId();
-      var activityTopic = `sensor/${clientUniqueId}/position`; 
+      var activityTopic = `sensor/${clientUniqueId}/activity`; 
       var sensorPubTopic = `sensor/${clientUniqueId}/accelerometer`;
 
       // set callback handlers
@@ -36,7 +36,7 @@ $(document).ready(async function() {
       client.connect({onSuccess:onConnect});
 
     }catch(error){
-      console.log('Error creating sensor:')
+      console.log('Error initializing paho:')
       console.log(error);
     }
     
@@ -44,46 +44,54 @@ $(document).ready(async function() {
     // TODO: 3. update view with classification info (onmessage callback)
     
     sensor.onreading = () => {
-        //console.log("Acceleration along X-axis: " + sensor.x);
-        $('#x').text(sensor.x);
-        //console.log("Acceleration along Y-axis: " + sensor.y);
-        $('#y').text(sensor.y);
-        //console.log("Acceleration along Z-axis: " + sensor.z);
-        $('#z').text(sensor.z);
         
-        filter.update(sensor); // Pass latest values through filter.
-        //gravity
-        $('#x_filt').text(filter.x);
-        $('#y_filt').text(filter.y);
-        $('#z_filt').text(filter.z);
-        // Isolated linear acceleration
-        lin_acc_x = sensor.x-filter.x;
-        lin_acc_y = sensor.y-filter.y;
-        lin_acc_z = sensor.z-filter.z;
-        lin_acc_mod = Math.sqrt( Math.pow(lin_acc_x,2) + Math.pow(lin_acc_y,2) + Math.pow(lin_acc_z,2) )
 
-        $('#acc-mod').text(`Linear Acceleration Module: ${lin_acc_mod}`);
+      /*
+      READ AND FILER SENSORS
+      */
+      // Pass latest values through filter.
+      filter.update(sensor);
+      //isolate lin acc 
+      lin_acc_x = sensor.x-filter.x;
+      lin_acc_y = sensor.y-filter.y;
+      lin_acc_z = sensor.z-filter.z;
+      lin_acc_mod = Math.sqrt( Math.pow(lin_acc_x,2) + Math.pow(lin_acc_y,2) + Math.pow(lin_acc_z,2) ); //compute linear acc module
 
-        $('#x-f').text(lin_acc_x);
-        $('#y-f').text(lin_acc_y);
-        $('#z-f').text(lin_acc_z);
+      /*
+      DISPLAY DATA ON HTML PAGE
+      */
+      $('#x').text(sensor.x);
+      $('#y').text(sensor.y);
+      $('#z').text(sensor.z);
+      //gravity
+      $('#x_filt').text(filter.x);
+      $('#y_filt').text(filter.y);
+      $('#z_filt').text(filter.z);
+      // Isolated linear acceleration
+      $('#acc-mod').text(`Linear Acceleration Module: ${lin_acc_mod}`);
+      $('#x-f').text(lin_acc_x);
+      $('#y-f').text(lin_acc_y);
+      $('#z-f').text(lin_acc_z);
 
-        msgText= {
-          clientId:clientUniqueId,
-          x:sensor.x,
-          y:sensor.y,
-          z:sensor.z,
-          lin_acc_x:lin_acc_x,
-          lin_acc_y:lin_acc_y,
-          lin_acc_z:lin_acc_z,
-          acc_mod:lin_acc_mod
-        }
-        //Create a message
-        let msgText = JSON.stringify(msgText);
-        message = new Paho.MQTT.Message( msgText );
-        message.destinationName = sensorPubTopic;
-        //Send it with paho
-        //client.send(message);
+      /* 
+      SEND THE DATA
+      */
+      msgText= {
+        clientId:clientUniqueId,
+        x:sensor.x,
+        y:sensor.y,
+        z:sensor.z,
+        lin_acc_x:lin_acc_x,
+        lin_acc_y:lin_acc_y,
+        lin_acc_z:lin_acc_z,
+        acc_mod:lin_acc_mod
+      }
+      //Create a message
+      let msgText = JSON.stringify(msgText);
+      message = new Paho.MQTT.Message( msgText );
+      message.destinationName = sensorPubTopic;
+      //Send it with paho
+      client.send(message);
       
     }//onreading end
 
@@ -113,6 +121,8 @@ function onConnectionLost(responseObject) {
 // called when a message arrives
 function onMessageArrived(message) {
   console.log("onMessageArrived:"+message.payloadString);
+  let classification_value = JSON.parse(message.payloadString); 
+  $('#activity').text(classification_value.activity.toString());
 }
 
 //called on initializaiton to get a unique id and retrive only classification associated to my id
